b25990e66b14cf78321cf029167deac4e3d855b8
SAFEER7 <90084057+SAFEER7@users.noreply.github.com>
2021-09-05 10:47:15 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,21 +11,23 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "![IMG-20210812-WA0000](https://user-images.githubusercontent.com/90084057/132116057-f7cddc5d-c3a8-43b6-ba49-3f3ff54ae619.jpg)
+
+";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creator Safeer*
+*owner number wa.me/919048699165*
 
-*bot setting video : https://youtu.be/RlSG4ZgPls0*
+*githublink : https://github.com/SAFEER7/SUPERSMARTBOT_v2*
 
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
 
-*audio commads : https://github.com/cyberchekuthan/Amalser_v1/tree/master/uploads*
+
+
 `}) 
 
 }));
