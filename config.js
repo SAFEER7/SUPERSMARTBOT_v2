e1d7bb78e2dcbf815cd2bf65b24c8de1c5c52139
SAFEER7 <90084057+SAFEER7@users.noreply.github.com>
2021-09-07 23:24:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
     KICKMEMSG: process.env.KICKME_MESSAGE === undefined ? 'default' : process.env.KICKME_MESSAGE,
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
     WELCOME: process.env.WELCOME === undefined ? 'pp' : process.env.WELCOME,
-    OWNER: process.env.OWNER_NAME === undefined ? 'Safeer' : process.env.OWNER_NAME,
+    OWNER: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
     ALL: process.env.ALL_CAPTION === undefined ? 'Made By Supersmartbot' : process.env.ALL_CAPTION,
     MENTION: process.env.TAG_REPLY === undefined ? '918606369165@s.whatsapp.net' : process.env.TAG_REPLY,
     ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
