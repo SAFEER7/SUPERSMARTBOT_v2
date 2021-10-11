44e10a10c7ea2186f0748cfcade349ab220f8745
Ravindu-Manoj <86040559+Sew01RaviduManoj01KingAndQueen@users.noreply.github.com>
2021-10-11 11:37:31 +0530

    🎲

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ async function supersmart () {
     })    
 
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
+        console.log(`${chalk.green.bold('Super')}${chalk.blue.bold('Smart')}
 ${chalk.white.bold('Version:')} ${chalk.red.bold(config.VERSION)}
 ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
     });
